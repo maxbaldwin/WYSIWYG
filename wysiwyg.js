b7a27b5e0cf194766d24d76ced03bf94e6bb7b9b
Max Baldwin <maxrbaldwin@gmail.com>
2016-03-12 11:43:09 -0600

    posting these updates

--- wysiwyg.js
+++ wysiwyg.js
@@ -13,44 +13,63 @@ let people = [
   }
 },
 {
-  title: "Samurai",
-  name: " Tomoe Gozen",
-  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+  title: "Director",
+  name: " Wes Anderson",
+  bio: "Director of 'Fantastic Mr. Fox', 'Darjeeling Limited', and 'Life Aquatic'.",
+  image: "http://cdn.collider.com/wp-content/uploads/wes-anderson1.jpg",
   lifespan: {
-    birth: 1747,
-    death: 1797
+    birth: 1969,
+    death: 2016
   }
 },
 {
-  title: "Samurai",
-  name: " Tomoe Gozen",
-  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+  title: "Topshelf Records Owner",
+  name: " Kevin Duquette",
+  bio: "Owner of my favorite record label.",
+  image: "https://41.media.tumblr.com/6d82d105d106f74500ef378240eb27f4/tumblr_n0eoriddhE1s0orrmo1_500.jpg",
   lifespan: {
-    birth: 1747,
-    death: 1797
+    birth: 1980,
+    death: 2016
   }
 }
 ]
 
 let container = document.getElementById("container");
 let input = document.getElementById("input");
-let card = document.getElementsByClassName("card");
+let card = document.getElementsByClassName("card"); //returning an array
 // console.log(card);
 
+function populateDom(){ //step one
 for (let i=0;  i < people.length; i++) {
   let person = people[i];
-  buildCard(person) 
+  buildCard(person); //all cards populate here on DOM
+}
+  addClickEvent();
 }
 
-function buildCard(person) {
+function buildCard(person) { //step two
   container.innerHTML += `<person class="card">
-  <header>${person.title}${person.name}</header><section>${person.bio}<img-src="${person.image}"></section>
+  <header>${person.title}${person.name}</header><section>${person.bio}<img src="${person.image}"></section>
   <footer>${person.lifespan.birth}-${person.lifespan.death}</footer></person>`
 }
 
+function addClickEvent(currentcard) { //step three
+    for(let i = 0; i < card.length; i++) {
+      let currentcard = card[i];
+      currentcard.addEventListener("click", function(){
+        removeSelected();
+        input.value = "";
+        input.focus();
+        currentcard.classList.add("selected");
+      })
+    }
+}
 
+function removeSelected () { //step four
+  for (let i=0; i < card.length; i++) {
+    card[i].classList.remove("selected");
+  }
+}
 
-
+populateDom();
 
